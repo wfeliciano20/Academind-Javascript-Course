b6ea745b63ebf03625cd51527778d851c3683c85
djwillo20 <djwillo20@hotmail.com>
2020-03-25 22:55:25 -0400

    assingnment 2

--- Academmind_Javascript_course/basics-assignment-2-start/assignment.js
+++ Academmind_Javascript_course/basics-assignment-2-start/assignment.js
@@ -1,1 +1,20 @@
 const task3Element = document.getElementById('task-3');
+
+function noParameter() {
+    alert("No Parameters");
+}
+
+function oneParameter(name) {
+    alert(name);
+}
+
+noParameter();
+oneParameter("max");
+
+task3Element.addEventListener('click', noParameter);
+
+function thePowerOFThree(one, two, three) {
+    return one + two + three;
+}
+
+alert(thePowerOFThree('hello', 'world', '!'));
