6ea9b36e58409504c615dfbf43aa9ea6b2da47a0
william feliciano <williamfeliciano@williams-MacBook-Pro.local>
2020-03-30 20:59:19 -0400

    functions assignment completed

--- Academmind_Javascript_course/functions-assignment-problem/assignment.js
+++ Academmind_Javascript_course/functions-assignment-problem/assignment.js
@@ -1,5 +1,33 @@
-function sayHello(name) {
-  console.log('Hi ' + name);
-}
+const sayHello = (name) => console.log('Hi ' + name);
+const sayHello2 = (name, phrase) => console.log(phrase + ' ' + name);
+const sayHello3 = () => console.log('Hi ');
+const sayHello4 = (name) => ('Hi ' + name);
+const sayHello5 = (name, phrase = 'hi') => console.log(phrase + ' ' + name);
+
 
 sayHello();
+sayHello2('max', 'hello');
+sayHello3();
+console.log(sayHello4('max'));
+
+function checkInput(callback, ...strings) {
+    let hasEmptyText = false;
+    for (const text of strings) {
+        if (!text) {
+            hasEmptyText = true;
+            break;
+        }
+    }
+    if (!hasEmptyText) {
+        callback();
+    }
+}
+
+checkInput(
+    () => {
+        console.log('All not empty!');
+    },
+    'Hello',
+    '12',
+    'adsfa',
+    'Not empty');
